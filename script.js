6ef28b62731eaabd217d261720d25abc7e86366c
Ranjan Nayak <swadhin.ranjannayak@siemens.com>
2020-05-17 12:30:07 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("check again and again")
+var btn = document.getElementById("veeksha");
+var div = document.getElementsByClassName("divclass")
+btn.addEventListener("click",function(){
+div.style.backgroundColor="yellow";
+})
